05c5348dcf02d4ed2311b0f1dcd8cbeb296adc84
DanielMikitiuk <dani4ku@gmail.com>
2025-03-26 15:35:09 +0100

    working pre-release no design

--- src/utils/get-joke.js
+++ src/utils/get-joke.js
@@ -8,13 +8,22 @@
 
 const getJoke = async (categories = ['Any'], blacklist = []) => {
     try {
-         // Only add blacklistFlags if there are any
-         const blacklistParam = blacklist.length > 0 ? `blacklistFlags=${blacklist.join(",")}&` : "";
+        // Only add blacklistFlags if there are any
+        const blacklistParam =
+            blacklist.length > 0
+                ? `blacklistFlags=${blacklist.join(',')}&`
+                : '';
 
-         //add category
-        const categoryParam = categories.join(","); 
+        //add category
+        if (categories.length === 0) {
+            categories = ['Any'];
+        }
+        const categoryParam = categories.join(',');
 
-        const joke = await fetch(`https://v2.jokeapi.dev/joke/${categoryParam}?${blacklistParam}format=txt`);
+        const joke = await fetch(
+            `https://v2.jokeapi.dev/joke/${categoryParam}?${blacklistParam}format=txt`
+        );
+
         if (joke.status !== 200) {
             throw new Error('Error fetching joke');
         }
